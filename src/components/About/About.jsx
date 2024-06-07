b0b055d9ae00900f7d8b4465d8e889456802eb55
Dylan Thrasher <thrasher_dylan@yahoo.com>
2024-06-07 00:54:00 -0400

    updated About section

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,5 +1,39 @@
 import React from "react";
+import styles from './About.module.css'
+import {getImageUrl} from '../../utils'
 
 export const About = () => {
-    return <div>About</div>
+    return (
+    <section className={styles.container} id="about">
+        <h2 className={styles.title}>About</h2>
+        <div className={styles.content}>
+            <img src={getImageUrl('about/aboutImage.png')} 
+            alt="Dylan's avatar writing in a notepad" 
+            className={styles.aboutImage}/>
+            <ul className={styles.aboutItems}>
+                <li className={styles.aboutItem}>
+                    <img src={getImageUrl('about/cursorIcon.png')} alt="Cursor Icon" />
+                    <div className={styles.aboutItemText}>
+                        <h3>Frontend Developer</h3>
+                        <p>I'm a frontend developer with experience in building responsive and optimized sites.</p>
+                    </div>
+                </li>
+                <li className={styles.aboutItem}>
+                    <img src={getImageUrl('about/serverIcon.png')} alt="Server Icon" />
+                    <div className={styles.aboutItemText}>
+                        <h3>Backend Developer</h3>
+                        <p>I have experience developing fast and optimized back-end systems and APIs.</p>
+                    </div>
+                </li>
+                <li className={styles.aboutItem}>
+                    <img src={getImageUrl('about/uiIcon.png')} alt="UI Icon" />
+                    <div className={styles.aboutItemText}>
+                        <h3>UI Developer</h3>
+                        <p>I have experience designing landing pages.</p>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </section>
+    )
 }
